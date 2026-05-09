1d715cf3a3f63e16b488044106854bd0828af61b
tankdonut <206458089+tankdonut@users.noreply.github.com>
2026-05-09 20:09:34 +0000

    chore: remove automated tools image bump workflow and task

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,9 +2,7 @@ from invoke import Collection
 
 import build
 import dev
-import update
 
 ns = Collection()
 ns.add_collection(Collection.from_module(build))
 ns.add_collection(Collection.from_module(dev))
-ns.add_collection(Collection.from_module(update))
